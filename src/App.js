c0a8d5989afe44b46b76b790481370ed7fcb415f
Taofiq <taofiqaiyelabegan45@gmail.com>
2021-04-16 10:26:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <h1>Welcome to React Parcel Micro App!</h1>
+       <p>Hard to get more minimal than this React app.</p>
+       <p>Hey I am creating tic tac toe game</p>
+    </div>
+  )
+}
+
+export default App
